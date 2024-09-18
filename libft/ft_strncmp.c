bfd2a4af1439b6da1dab1367cd789c89e09843d1
Daniel Jose Pereira <78903332+danijp03@users.noreply.github.com>
2024-09-18 23:43:59 +0200

    Update ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((*s1 != '\0' && *s2 != '\0') && i < n)
 	{
 		if (*s1 == *s2)
-			return(ft_strncmp(s1 + 1, s2 + 1, i - 1));
+			return(ft_strncmp(s1 + 1, s2 + 1, n - 1));
 		if (*s1 < *s2)
 			return (-1);
 		if (*s1 > *s2)
